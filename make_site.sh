2ce40f5b7191697107d394e387bb33589d28e3b8
4loops <colpeterson@gmail.com>
2019-11-26 13:43:27 -0800

    Added 7 and 8 players

--- make_site.sh
+++ make_site.sh
@@ -1,4 +1,6 @@
 python make_table.py 4 > salad-4.html
 python make_table.py 5 > salad-5.html
 python make_table.py 6 > salad-6.html
+python make_table.py 7 > salad-7.html
+python make_table.py 8 > salad-8.html
 
